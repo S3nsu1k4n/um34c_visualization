9d7a3be4d0dd7dc4006d8fcb2cc109d8e56d2c52
S3nsu1k4n <saschakoeppe@gmail.com>
2022-06-11 03:10:04 +0200

    implemented search nearby of bluetooth devices and added functions to the api

--- app/main.py
+++ app/main.py
@@ -1,15 +1,27 @@
 import os
-from fastapi import FastAPI
+import time
+from fastapi import FastAPI, Request
 
 from config import ServerConfig
 
+from .routers import bl_connection
+
 app = FastAPI()
+app.include_router(bl_connection.router)
 
 
 @app.get('/')
 async def root():
     return {'message': 'Hello World'}
 
+@app.middleware('http')
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    print(process_time)
+    response.headers['X-Process-Time'] = str(process_time)
+    return response
 
 if __name__ == '__main__':
     os.system(f'uvicorn main:app '
--- config.py
+++ config.py
@@ -8,3 +8,7 @@ load_dotenv(join(dirname(__file__), '.env'))
 class ServerConfig(str, Enum):
     HOST = environ.get('HOST') or '127.0.0.1'
     PORT = environ.get('PORT') or '8080'
+
+
+class UM34CConfig(str, Enum):
+    MACADDRESS = environ.get('MACADDRESS') or ''
